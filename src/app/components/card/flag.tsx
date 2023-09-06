4dae654c25131231bf65c2bb19a46e8d039b6bb2
Samuel Kitazume <samuel.kitazume@gmail.com>
2023-09-06 17:38:05 -0400

    Remove lazy load from country flags

--- src/app/components/card/flag.tsx
+++ src/app/components/card/flag.tsx
@@ -7,8 +7,9 @@ export interface FlagProperties {
 
 export default function Flag({ url, alt }: FlagProperties) {
   return (
-    <div className="relative self-center w-72 h-48 mt-12 mb-8 rounded-xl overflow-hidden bg-gradient-to-t from-indigo-500 via-purple-500 to-pink-500 shadow-lg shadow-zinc-950">
+    <div className="relative mb-8 mt-12 h-48 w-72 self-center overflow-hidden rounded-xl bg-gradient-to-t from-indigo-500 via-purple-500 to-pink-500 shadow-lg shadow-zinc-950">
       <Image
+        priority
         className="object-cover"
         src={url}
         fill={true}
